13a1c1b6d881dee44b1f577a31208e98b6fe87eb
Jeremiah LaForge <laforge.jeremiah.s@gmail.com>
2018-09-17 11:44:57 -0400

    dao updates

--- src/main/java/com/revature/dao/CustomerDAO.java
+++ src/main/java/com/revature/dao/CustomerDAO.java
@@ -1,6 +1,19 @@
 package com.revature.dao;
 
+import java.util.List;
+
+import com.revature.models.RUser;
+
 public interface CustomerDAO {
 
+	public List<RUser> getAllUsers();
+	public List<RUser> getAllChefs();
+	public List<RUser> getAllNonChefs();
+	public RUser getUserByUserName();
+	public void createUser(RUser user);
+	public void deleteUser(RUser user);
+	public void updateUser(RUser user);
+	
+	
 	
 }
