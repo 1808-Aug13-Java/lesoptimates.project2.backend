a74c2de44bd88351596d27da97b78c9a5142c3f9
Jeremiah LaForge <laforge.jeremiah.s@gmail.com>
2018-09-17 13:33:27 -0400

    Oops, made some bad names

--- src/main/java/com/revature/dao/UserDAO.java
+++ src/main/java/com/revature/dao/UserDAO.java
@@ -4,12 +4,12 @@ import java.util.List;
 
 import com.revature.models.RUser;
 
-public interface CustomerDAO {
+public interface UserDAO {
 
 	public List<RUser> getAllUsers();
 	public List<RUser> getAllChefs();
 	public List<RUser> getAllNonChefs();
-	public RUser getUserByUserName();
+	public RUser getUserByUserName(String userName);
 	public void createUser(RUser user);
 	public void deleteUser(RUser user);
 	public void updateUser(RUser user);
