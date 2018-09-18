dc1f872c8e5751a14a4304bde4e805776eda50cd
Jeremiah LaForge <laforge.jeremiah.s@gmail.com>
2018-09-18 15:04:34 -0400

    driver changes

--- src/main/java/com/revature/util/Driver.java
+++ src/main/java/com/revature/util/Driver.java
@@ -18,28 +18,26 @@ public class Driver {
 		  */
 		Session s = HibernateUtil.getSession();
 		s.close();
-		 /*	
 		RecipeDaoImpl rdi = new RecipeDaoImpl();
 		Recipe recipe1 = new Recipe("12345 test");
 		
 		int pk = rdi.saveRecipe(recipe1);
 		log.info("Stuff " + recipe1.toString());
-		*/
 //		recipe1.setRecipeId(pk);
-//		
+	
 //		recipe1.setrecipeJSON("09876 test");
-//		
+
 //		rdi.mergeRecipe(recipe1);
 //		log.info(rdi.loadRecipe(recipe1.getRecipeId()));
 //		log.info(rdi.loadRecipe(recipe1.getRecipeId()));
 //		log.info(rdi.getRecipe(recipe1.getRecipeId()));
-//		
+
 //		recipe1.setrecipeJSON("56787 test");
-//		
+
 //		rdi.updateRecipe(recipe1);
-//		
+
 //		Recipe recipe2 = new Recipe("persist test");
-//		
+
 //		rdi.persistRecipe(recipe2);
 		
 	}
