16ed1e84a788340fcdab09bca63ee9b2110edf75
Jeremiah LaForge <laforge.jeremiah.s@gmail.com>
2018-09-26 16:25:20 -0400

    got update from johnathan

--- src/main/java/com/revature/controllers/LoginController.java
+++ src/main/java/com/revature/controllers/LoginController.java
@@ -1,23 +1,28 @@
 package com.revature.controllers;
 
 
+import java.util.Collections;
+import java.util.Map;
 
-
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.models.RUser;
 import com.revature.services.UserService;
 
 
-
+@Controller
 @RestController
 @CrossOrigin
 public class LoginController {
@@ -28,7 +33,7 @@ public class LoginController {
 	private LoginController() {
 		super();
 	}
-
+	
 	
 	@RequestMapping(value = { "/login" }, method = RequestMethod.POST)
 	public RUser login(@RequestParam("userName") String username, @RequestParam("pswd") String password, HttpSession session) {
@@ -37,7 +42,6 @@ public class LoginController {
 	    if(user!=null) {	    	
 	        session.setAttribute("USER", user);
 	        log.info((RUser) session.getAttribute("USER"));
-
 	    } else {
 	        return null;
 	    }
