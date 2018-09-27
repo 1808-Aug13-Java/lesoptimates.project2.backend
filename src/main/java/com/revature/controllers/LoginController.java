70e2729df903777cb6610750e74977356c846410
Jeremiah LaForge <laforge.jeremiah.s@gmail.com>
2018-09-27 13:19:36 -0400

    Merged Johnathan's changes into master

--- src/main/java/com/revature/controllers/LoginController.java
+++ src/main/java/com/revature/controllers/LoginController.java
@@ -1,20 +1,25 @@
 package com.revature.controllers;
 
 
+
 import javax.servlet.http.HttpSession;
 
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.models.RUser;
 import com.revature.services.UserService;
 
 
 @Controller
+@RestController
+@CrossOrigin
 public class LoginController {
 
 	private static Logger log = Logger.getRootLogger();
@@ -24,16 +29,38 @@ public class LoginController {
 		super();
 	}
 	
+	
 	@RequestMapping(value = { "/login" }, method = RequestMethod.POST)
-	@ResponseBody
-	public RUser login(HttpSession session, @RequestParam("userName") String username, @RequestParam("pswd")String password) {
+	public RUser login(@RequestParam("userName") String username, @RequestParam("pswd") String password, HttpSession session) {
+		
 	    RUser user = userService.authenticateUser(username, password);
-	    if(user!=null) {
+	    if(user!=null) {	    	
 	        session.setAttribute("USER", user);
+	        log.info((RUser) session.getAttribute("USER"));
 	    } else {
 	        return null;
 	    }
 	    return user;
+	}
+	
+	
+	@GetMapping("/session")
+	public RUser getSession(HttpSession session) {
+		if(session != null) {
+			log.info((RUser) session.getAttribute("USER"));
+			return (RUser) session.getAttribute("USER");
+		}
+	    return null;
+	}
+	
+	  @GetMapping("/logout")
+	  public void logout(HttpSession session) {
+	    session.invalidate();
+	  }
+
+	@GetMapping("/login")
+	public String getLogin() {
+		return "Login";
 	}
 
 }
